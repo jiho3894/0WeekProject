a22bb018f8f1404da05c147ca7091cb918de2b13
김지호 <crsn1111@gmail.com>
2022-02-24 20:58:50 +0900

    Delete WATCHING directory

--- WATCHING/static/js/favorites.js
+++
@@ -1,7 +0,0 @@
-$(".navbar_userMenu").click(() => {
-  $(".detailBox").slideToggle(100);
-});
-
-const favorite = () => {
-  alert("즐겨찾기를 삭제해 봤습니다");
-};
--- WATCHING/static/js/login.js
+++
@@ -1,9 +0,0 @@
-// 첫 시작 로그인 input에 focus
-$(document).ready(() => {
-  $("#id").focus();
-});
-
-const login = () => {
-  alert("로그인 완료");
-  location.href = "main.html";
-};
